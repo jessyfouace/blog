cfb94302cf3d25e57ab6cecc93d2642b2a2dad16
Jessy Fouace <jessy.fouace62@gmail.com>
2018-09-13 14:39:33 +0200

    blog ok, with form for post but no commentary

--- viewcom.php
+++ viewcom.php
@@ -1,9 +1,10 @@
 <?php include('config.php'); ?>
+
 <!doctype html>
 <html class="no-js" lang="fr">
 
 <head>
-  <title></title>
+  <title>Commentaire</title>
   <meta charset="utf-8">
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,39 +16,32 @@
 </head>
 
 <body>
-  <form class="col-12 text-center pb-2 pt-2" action="addblog.php" method="post">
-    <input type="submit" name="addblog" value="Ajouter page blog">
-  </form>
-
-  <div class="col-6 mx-auto">
-    <p>Les 5 derniers Billets du blog:</p>
-  </div>
-
   <?php
-   $reponse = $bdd->query('SELECT * FROM blogcommentaire ORDER BY id DESC LIMIT 5');
+  $blognumber = $_GET["blog"];
+  $test = $bdd->prepare('SELECT * FROM blogcommentaire WHERE id=?');
+  $test->execute(array($_GET['blog']));
 
-    echo '<div class="col-6 mx-auto">';
-     foreach ($reponse as $key => $value) {
-       echo "<a class='col-6 mx-auto' href='viewcom.php?blog=" . $value['id'] . "'>
-       <div class='col-12 mx-auto blogcreate mt-2'>
+  if ($test->rowCount() ==1) {
+    $test = $test->fetch();
+    $title = $test['title'];
+    $pseudo = $test['pseudo'];
+    $message = $test['message'];
+  }
+
+       echo "<div class='col-6 mx-auto blogcreate mt-2'>
          <div class='col-12 titlecom'>
-           <h1>" . $value['title'] . "</h1>
+           <h1>"; echo $title; echo "</h1>
            <p>test</p>
          </div>
          <div class='col-12 row'>
            <div class='col-12 name m-0'>
-             <h2>" . $value['pseudo'] . "</h2>
+             <h2>"; echo $pseudo; echo "</h2>
            </div>
            <div class='col-12 pl-4 pt-2'>
-             <p>" . $value['message'] . "</p>
+             <p>"; echo $message; echo "</p>
            </div>
          </div>
-       </div></a>";
-     }
-     echo '</div>';
-
-     $reponse->closeCursor();
-
+       </div>";
   ?>
 
 
