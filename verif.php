ec6859dc7abbc5bcd349fd7617a37c817f183e05
Jessy Fouace <jessy.fouace62@gmail.com>
2018-09-14 14:31:13 +0200

    Add some blog + com for send the sql

--- verif.php
+++ verif.php
@@ -5,8 +5,10 @@ $title = addslashes(strip_tags($_POST["title"]));
 $pseudo = addslashes(strip_tags($_POST["pseudo"]));
 $message = addslashes(strip_tags($_POST["message"]));
 
-if ($title !== "" && $pseudo !== "" && $message !== "") {
+if ($title !== "" AND $pseudo !== "" AND $message !== "") {
   $reponse = $bdd->exec("INSERT INTO blogcommentaire (title, pseudo, message) VALUES ('$title', '$pseudo', '$message')");
+  header('location: index.php');
+} else {
   header('location: index.php');
 }
 ?>
